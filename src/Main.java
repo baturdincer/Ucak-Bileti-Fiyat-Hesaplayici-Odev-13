84ebaa620db8aa016835b69bf3ab215f731e3406
baturdincer <baturdincerr@gmail.com>
2023-03-09 20:17:16 +0300

    first commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,38 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scanner= new Scanner(System.in);
+        System.out.print("Gideceğiniz mesafe(km): ");
+        double distance=scanner.nextDouble();
+        System.out.print("Yaşınız: ");
+        int age= scanner.nextInt();
+        System.out.println("Yolculuk türünü seçiniz:\n1- Tek yön uçak bileti\n2- Gidiş-dönüş uçak bileti");
+        int flightType= scanner.nextInt();
+
+        double pricePerKm= flightType==1? 0.10 : 0.20;
+        double price= distance*pricePerKm;
+
+        if(age<0||distance<0||flightType<1||flightType>2){
+            System.out.println("Hatalı bilgi girdiniz!");
+        }
+        else{
+            if(age<12){
+                price /=2;
+            }
+            else if(age<=24){
+                price= (price*90)/100;
+            }
+            else if(age>65){
+                price=(price*70)/100;
+            }
+
+            if (flightType==2){
+                price=(price*80/100);
+            }
+            System.out.println("Toplam tutar: "+price);
+        }
+
+
     }
 }
